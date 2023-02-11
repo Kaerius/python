29a53c8c8cb7a4007c87a31e9c56864b85985dde
Kaerius <kddkda@mail.ru>
2023-02-11 17:06:45 +0500

    Update JourneyMap_Import-Export_Map.py

--- JourneyMap_Import-Export_Map.py
+++ JourneyMap_Import-Export_Map.py
@@ -11,6 +11,9 @@ import PySimpleGUI as sg
 import os
 from PIL import Image
 import shutil
+from pathlib import Path
+
+progress_bar_counter = 0
 
 # Рисуем кнопки и окна
 layout = [
@@ -18,13 +21,32 @@ layout = [
      ],
     [sg.Text('Папка импорта '), sg.InputText('Папка куда нужно дополнить'), sg.FolderBrowse(),
      ],
+    [sg.ProgressBar(max_value=265, orientation='h', size=(44, 20), key='progressbar')],
     [sg.Output(size=(67, 15))],
     [sg.Submit('Применить'), sg.Cancel('Выход')]
 ]
 window = sg.Window('Import-Export JourneyMap', layout)
+progress_bar = window['progressbar']
+
+# Обидиненить или скопировать файл
+def merging_images(img_path1,img_path2):
+    if os.path.exists(img_path2):
+        print('Дополнен:',img_path2,'->',img_path1)
+        img1 = Image.open(img_path1)
+        img2 = Image.open(img_path2)
+        img1.paste(img2, (0,0), img2)
+        img1.save(img_path2)
+        img1.close()
+        img2.close()
+        window.Refresh()
+    else:
+        print('Скопирован:',img_path1,'->',img_path2)
+        shutil.copyfile(img_path1, img_path2)
+        window.Refresh()
 
 # Рекурсивный перебор папки
 def folder_crawl(path,path2):
+    global progress_bar_counter
     for i in os.listdir(path):
         if not os.path.isdir(path2):
             os.mkdir(path2)
@@ -35,20 +57,8 @@ def folder_crawl(path,path2):
         else:
             if i.endswith('.png'):
                 merging_images(path+'\\'+i,path2+'\\'+i)
-
-# Обидиненить или скопировать файл
-def merging_images(img_path1,img_path2):
-    if os.path.exists(img_path2):
-        print('Дополнен:',img_path2)
-        img1 = Image.open(img_path1)
-        img2 = Image.open(img_path2)
-        img1.paste(img2, (0,0), img2)
-        img1.save(img_path2)
-        img1.close()
-        img2.close()
-    else:
-        print('Скопирован:',img_path1)
-        shutil.copyfile(img_path1, img_path2)
+                progress_bar_counter = progress_bar_counter + 1
+                progress_bar.UpdateBar(progress_bar_counter, progress_bar_max)
 
 # Основной цикл программы
 while True: 
@@ -56,8 +66,12 @@ while True:
     if event in (None, 'Exit', 'Cancel', 'Выход'):
         break
     if event in ('Применить', 'Submit'):
-        if values[0] and values[1]:
-            folder_crawl(values[0],values[1])  
+        if os.path.isdir(values[0]) and os.path.isdir(values[1]):
+            print('Из ',values[0])
+            print('В ',values[1])
+            progress_bar_max=len(list(Path(values[0]).rglob("*.png")))
+            folder_crawl(values[1],values[0])
+            print('Завершено!')
         else:
-            print('Пожалуйста, выберите вторую папку.')
+            print('Выбраны не папки.')
 window.close()
